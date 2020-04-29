e9fade55cd6b9e567bcb17561f3fcaf68578f4b9
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-29 17:11:54 +0200

    refactor

---
+++ Brain_tumor/BrainCancerDetection.py
@@ -0,0 +1,165 @@
+import numpy as np
+from tqdm import tqdm
+import cv2
+import os
+import shutil
+import itertools
+import imutils
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import plotly.graph_objs as go
+from plotly.offline import init_notebook_mode, iplot
+from plotly import tools
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras import layers
+from keras.models import Model, Sequential
+from keras.optimizers import Adam, RMSprop
+from keras.callbacks import EarlyStopping
+
+init_notebook_mode(connected=True)
+RANDOM_SEED = 123
+
+
+IMG_PATH = '../Brain_tumor/brain_tumor_dataset/'
+# split the data by train/val/test
+for CLASS in os.listdir(IMG_PATH):
+    if not CLASS.startswith('.'):
+        IMG_NUM = len(os.listdir(IMG_PATH + CLASS))
+        print(IMG_NUM)
+        for (n, FILE_NAME) in enumerate(os.listdir(IMG_PATH + CLASS)):
+            img = IMG_PATH + CLASS + '/' + FILE_NAME
+            if n < 5:
+                shutil.copy(img, 'TEST/' + CLASS.upper() + '/' + FILE_NAME)
+            elif n < 0.8*IMG_NUM:
+                shutil.copy(img, 'TRAIN/' + CLASS.upper() + '/' + FILE_NAME)
+            else:
+                shutil.copy(img, 'VAL/' + CLASS.upper() + '/' + FILE_NAME)
+
+"""
+import data and preprocessing
+"""
+
+
+def load_data(dir_path, img_size=(100, 100)):
+    """
+    Load resized images as np.arrays to workspace
+    """
+    X = []
+    y = []
+    i = 0
+    labels = dict()
+    for path in tqdm(sorted(os.listdir(dir_path))):
+        if not path.startswith('.'):
+            labels[i] = path
+            for file in os.listdir(dir_path + path):
+                if not file.startswith('.'):
+                    img = cv2.imread(dir_path + path + '/' + file)
+                    X.append(img)
+                    y.append(i)
+            i += 1
+    X = np.array(X)
+    y = np.array(y)
+    print(f'{len(X)} images loaded from {dir_path} directory.')
+    return X, y, labels
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize=(6, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    cm = np.round(cm, 2)
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+TRAIN_DIR = 'TRAIN/'
+TEST_DIR = 'TEST/'
+VAL_DIR = 'VAL/'
+IMG_SIZE = (224, 224)
+
+# use predefined function to load the image data into workspace
+X_train, y_train, labels = load_data(TRAIN_DIR, IMG_SIZE)
+X_test, y_test, _ = load_data(TEST_DIR, IMG_SIZE)
+X_val, y_val, _ = load_data(VAL_DIR, IMG_SIZE)
+
+
+y = dict()
+y[0] = []
+y[1] = []
+for set_name in (y_train, y_val, y_test):
+    y[0].append(np.sum(set_name == 0))
+    y[1].append(np.sum(set_name == 1))
+
+trace0 = go.Bar(
+    x=['Train Set', 'Validation Set', 'Test Set'],
+    y=y[0],
+    name='No',
+    marker=dict(color='#33cc33'),
+    opacity=0.7
+)
+trace1 = go.Bar(
+    x=['Train Set', 'Validation Set', 'Test Set'],
+    y=y[1],
+    name='Yes',
+    marker=dict(color='#ff3300'),
+    opacity=0.7
+)
+data = [trace0, trace1]
+layout = go.Layout(
+    title='Count of classes in each set',
+    xaxis={'title': 'Set'},
+    yaxis={'title': 'Count'}
+)
+fig = go.Figure(data, layout)
+
+
+def plot_samples(X, y, labels_dict, n=50):
+    """
+    Creates a gridplot for desired number of images (n) from the specified set
+    """
+    for index in range(len(labels_dict)):
+        imgs = X[np.argwhere(y == index)][:n]
+        j = 10
+        i = int(n/j)
+
+        plt.figure(figsize=(15, 6))
+        c = 1
+        for img in imgs:
+            plt.subplot(i, j, c)
+            plt.imshow(img[0])
+
+            plt.xticks([])
+            plt.yticks([])
+            c += 1
+        plt.suptitle('Tumor: {}'.format(labels_dict[index]))
+        plt.show()
+
+
+plot_samples(X_train, y_train, labels, 30)
+
