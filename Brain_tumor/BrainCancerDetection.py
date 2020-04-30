bdb9b0c4996b986022d251c828f0da9e9696a31d
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-30 17:50:39 +0200

    update

--- Brain_tumor/BrainCancerDetection.py
+++ Brain_tumor/BrainCancerDetection.py
@@ -161,5 +161,390 @@ def plot_samples(X, y, labels_dict, n=50):
         plt.show()
 
 
+"""
+
+
+As you can see, images have different width and height and diffent size of "black corners". 
+Since the image size for VGG-16 imput layer is (224,224) some wide images may look weird after resizing. 
+Histogram of ratio distributions (ratio = width/height):
+
+"""
 plot_samples(X_train, y_train, labels, 30)
 
+RATIO_LIST = []
+for set in (X_train, X_test, X_val):
+    for img in set:
+        RATIO_LIST.append(img.shape[1]/img.shape[0])
+
+plt.hist(RATIO_LIST)
+plt.title('Distribution of Image Ratios')
+plt.xlabel('Ratio Value')
+plt.ylabel('Count')
+plt.savefig('histogram.png')
+
+'''
+The first step of "normalization" would be to crop the brain out of the images. 
+'''
+
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+    set_new = []
+    for img in set_name:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # threshold the image, then perform a series of erosions +
+        # dilations to remove any small regions of noise
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in thresholded image, then grab the largest one
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+
+        # find the extreme points
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS,
+                      extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        set_new.append(new_img)
+
+    return np.array(set_new)
+
+# Let's look at example what this function will do with MRI scans:
+
+
+img = cv2.imread(
+    '/home/eddie/Desktop/python/medical_analysis_machine_learning/Brain_tumor/brain_tumor_dataset/yes/Y108.jpg')
+img = cv2.resize(
+    img,
+    dsize=IMG_SIZE,
+    interpolation=cv2.INTER_CUBIC
+)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# threshold the image, then perform a series of erosions +
+# dilations to remove any small regions of noise
+thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.erode(thresh, None, iterations=2)
+thresh = cv2.dilate(thresh, None, iterations=2)
+
+# find contours in thresholded image, then grab the largest one
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+c = max(cnts, key=cv2.contourArea)
+
+# find the extreme points
+extLeft = tuple(c[c[:, :, 0].argmin()][0])
+extRight = tuple(c[c[:, :, 0].argmax()][0])
+extTop = tuple(c[c[:, :, 1].argmin()][0])
+extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+# add contour on the image
+img_cnt = cv2.drawContours(img.copy(), [c], -1, (0, 255, 255), 4)
+
+# add extreme points
+img_pnt = cv2.circle(img_cnt.copy(), extLeft, 8, (0, 0, 255), -1)
+img_pnt = cv2.circle(img_pnt, extRight, 8, (0, 255, 0), -1)
+img_pnt = cv2.circle(img_pnt, extTop, 8, (255, 0, 0), -1)
+img_pnt = cv2.circle(img_pnt, extBot, 8, (255, 255, 0), -1)
+
+# crop
+ADD_PIXELS = 0
+new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS,
+              extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+plt.figure(figsize=(15, 6))
+plt.subplot(141)
+plt.imshow(img)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 1. Get the original image')
+plt.subplot(142)
+plt.imshow(img_cnt)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 2. Find the biggest contour')
+plt.subplot(143)
+plt.imshow(img_pnt)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 3. Find the extreme points')
+plt.subplot(144)
+plt.imshow(new_img)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 4. Crop the image')
+plt.show()
+
+
+# apply this for each set
+X_train_crop = crop_imgs(set_name=X_train)
+X_val_crop = crop_imgs(set_name=X_val)
+X_test_crop = crop_imgs(set_name=X_test)
+
+plot_samples(X_train_crop, y_train, labels, 30)
+
+
+def save_new_images(x_set, y_set, folder_name):
+    i = 0
+    for (img, imclass) in zip(x_set, y_set):
+        if imclass == 0:
+            cv2.imwrite(folder_name+'NO/'+str(i)+'.jpg', img)
+        else:
+            cv2.imwrite(folder_name+'YES/'+str(i)+'.jpg', img)
+        i += 1
+
+
+# saving new images to the folder
+# mkdir TRAIN_CROP TEST_CROP VAL_CROP TRAIN_CROP/YES TRAIN_CROP/NO TEST_CROP/YES TEST_CROP/NO VAL_CROP/YES VAL_CROP/NO
+
+save_new_images(X_train_crop, y_train, folder_name='TRAIN_CROP/')
+save_new_images(X_val_crop, y_val, folder_name='VAL_CROP/')
+save_new_images(X_test_crop, y_test, folder_name='TEST_CROP/')
+
+# The next step would be resizing images to (224,224) and applying preprocessing needed for VGG-16 model input.
+
+
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(
+            img,
+            dsize=img_size,
+            interpolation=cv2.INTER_CUBIC
+        )
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+
+X_train_prep = preprocess_imgs(set_name=X_train_crop, img_size=IMG_SIZE)
+X_test_prep = preprocess_imgs(set_name=X_test_crop, img_size=IMG_SIZE)
+X_val_prep = preprocess_imgs(set_name=X_val_crop, img_size=IMG_SIZE)
+
+# plot_samples(X_train_prep, y_train, labels, 30)
+
+
+'''
+
+4. CNN Model
+
+I was using Transfer Learning with VGG-16 architecture and weights as a base model.
+
+Since I had small data set I used the technique called Data Augmentation which helps to "increase" the size of training set.
+
+That's the example from one image how does augmentation look like.
+'''
+# set the paramters we want to change randomly
+demo_datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.05,
+    height_shift_range=0.05,
+    rescale=1./255,
+    shear_range=0.05,
+    brightness_range=[0.1, 1.5],
+    horizontal_flip=True,
+    vertical_flip=True
+)
+
+# os.mkdir('preview')
+x = X_train_crop[0]
+x = x.reshape((1,) + x.shape)
+
+i = 0
+for batch in demo_datagen.flow(x, batch_size=1, save_to_dir='preview', save_prefix='aug_img', save_format='jpg'):
+    i += 1
+    if i > 20:
+        break
+plt.imshow(X_train_crop[0])
+plt.xticks([])
+plt.yticks([])
+plt.title('Original Image')
+plt.show()
+
+plt.figure(figsize=(15, 6))
+i = 1
+for img in os.listdir('preview/'):
+    img = cv2.cv2.imread('preview/' + img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.subplot(3, 7, i)
+    plt.imshow(img)
+    plt.xticks([])
+    plt.yticks([])
+    i += 1
+    if i > 3*7:
+        break
+plt.suptitle('Augemented Images')
+plt.show()
+
+TRAIN_DIR = 'TRAIN_CROP/'
+VAL_DIR = 'VAL_CROP/'
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    brightness_range=[0.5, 1.5],
+    horizontal_flip=True,
+    vertical_flip=True,
+    preprocessing_function=preprocess_input
+)
+
+test_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input
+)
+
+
+train_generator = train_datagen.flow_from_directory(
+    TRAIN_DIR,
+    color_mode='rgb',
+    target_size=IMG_SIZE,
+    batch_size=32,
+    class_mode='binary',
+    seed=RANDOM_SEED
+)
+
+
+validation_generator = test_datagen.flow_from_directory(
+    VAL_DIR,
+    color_mode='rgb',
+    target_size=IMG_SIZE,
+    batch_size=16,
+    class_mode='binary',
+    seed=RANDOM_SEED
+)
+'''
+50 images loaded from VAL/ directory.
+Found 193 images belonging to 2 classes.
+Found 50 images belonging to 2 classes.
+'''
+# load base model
+vgg16_weight_path = '/home/eddie/Desktop/python/medical_analysis_machine_learning/Brain_tumor/Pretrained_models/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+base_model = VGG16(
+    weights=vgg16_weight_path,
+    include_top=False,
+    input_shape=IMG_SIZE + (3,)
+)
+
+NUM_CLASSES = 1
+
+model = Sequential()
+model.add(base_model)
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(NUM_CLASSES, activation='sigmoid'))
+
+model.layers[0].trainable = False
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=RMSprop(lr=1e-4),
+    metrics=['accuracy']
+)
+
+model.summary()
+
+
+'''
+Model: "sequential_1"
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+vgg16 (Model)                (None, 7, 7, 512)         14714688  
+_________________________________________________________________
+flatten_1 (Flatten)          (None, 25088)             0         
+_________________________________________________________________
+dropout_1 (Dropout)          (None, 25088)             0         
+_________________________________________________________________
+dense_1 (Dense)              (None, 1)                 25089     
+=================================================================
+Total params: 14,739,777
+Trainable params: 25,089
+Non-trainable params: 14,714,688
+'''
+EPOCHS = 30
+es = EarlyStopping(
+    monitor='val_acc',
+    mode='max',
+    patience=6
+)
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=50,
+    epochs=EPOCHS,
+    validation_data=validation_generator,
+    validation_steps=25,
+    callbacks=[es]
+)
+
+
+# 4.3. Model Performance
+
+# plot model performance
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs_range = range(1, len(history.epoch) + 1)
+
+plt.figure(figsize=(15, 5))
+
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Train Set')
+plt.plot(epochs_range, val_acc, label='Val Set')
+plt.legend(loc="best")
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.title('Model Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Train Set')
+plt.plot(epochs_range, val_loss, label='Val Set')
+plt.legend(loc="best")
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Model Loss')
+
+plt.tight_layout()
+plt.show()
+
+
+# validate on val set
+predictions = model.predict(X_val_prep)
+predictions = [1 if x > 0.5 else 0 for x in predictions]
+
+accuracy = accuracy_score(y_val, predictions)
+print('Val Accuracy = %.2f' % accuracy)
+
+confusion_mtx = confusion_matrix(y_val, predictions)
+cm = plot_confusion_matrix(
+    confusion_mtx, classes=list(labels.items()), normalize=False)
+
+ind_list = np.argwhere((y_test == predictions) == False)[:, -1]
+if ind_list.size == 0:
+    print('There are no missclassified images.')
+else:
+    for i in ind_list:
+        plt.figure()
+        plt.imshow(X_test_crop[i])
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(
+            f'Actual class: {y_val[i]}\nPredicted class: {predictions[i]}')
+        plt.show()
